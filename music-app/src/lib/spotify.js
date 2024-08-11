66a4a58179558da6b916da51fd91aa2d6020362f
hirokii97 <basket100713hiroki.ch@gmail.com>
2024-08-11 17:41:37 +0900

    add prettier

--- music-app/src/lib/spotify.js
+++ music-app/src/lib/spotify.js
@@ -1,17 +1,17 @@
-export const getSpotifyToken = async() => {
-    const url = "https://accounts.spotify.com/api/token"
-    const body = new URLSearchParams({
-        grant_type: "client_credentials",
-        client_id: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
-        client_secret: process.env.REACT_APP_SPOTIFY_CLIENT_SECRET,
-    })
-    const response = await fetch(url,{
-        method: "POST",
-        headers: {
-            'Content-Type' : 'application/x-www-form-urlencoded'
-        },
-        body: body.toString(),
-    })
-    const data = await response.json()
-    console.log(data);   
-}
+export const getSpotifyToken = async () => {
+  const url = "https://accounts.spotify.com/api/token";
+  const body = new URLSearchParams({
+    grant_type: "client_credentials",
+    client_id: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
+    client_secret: process.env.REACT_APP_SPOTIFY_CLIENT_SECRET,
+  });
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: body.toString(),
+  });
+  const data = await response.json();
+  console.log(data);
+};
